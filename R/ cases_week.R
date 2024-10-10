c6a317e83536b455ebbea41e25674d6750452956
fdzul <felipe.dzul.m@gmail.com>
2024-10-10 13:20:10 -0600

    this commit update the pkg

---
+++ R/ cases_week.R
@@ -0,0 +1,118 @@
+#' Estimated, probable, confirmed and hospitalized cases of dengue per week
+#'
+#' @param path is the path of the sinave files
+#' @param mpo is the name of minicipality
+#' @param year is the year the dataset
+#'
+#' @return a ggplot2 object
+#' @export
+#'
+#' @examples cases_week(path = path_sinave, mpo = "SILAO GTO", year = 2024)
+cases_week <- function(path, mpo, year){
+
+    a <- densnv::read(path = path,
+                      vbd = "DEN",
+                      complete = TRUE) |>
+        dplyr::filter(ANO == year) |>
+        dplyr::filter(ESTATUS_CASO  %in% c(1, 2)) |>
+        dplyr::filter(DES_MPO_REP %in% c(mpo)) |>
+        dplyr::group_by(SEM, MANEJO) |>
+        dplyr::summarise(n = dplyr::n(),
+                         .groups = "drop") |>
+        dplyr::mutate(MANEJO = ifelse(MANEJO == 1,
+                                      "Hospitalizado",
+                                      "Ambulatorio")) |>
+        ggplot2::ggplot() +
+        ggplot2::geom_line(ggplot2::aes(x = SEM,
+                                        y = n,
+                                        col = MANEJO),
+                           linewidth = 2) +
+        ggplot2::geom_point(ggplot2::aes(x = SEM,
+                                         y = n,
+                                         fill = MANEJO),
+                            color = "white",
+                            shape = 21,
+                            size = 5) +
+        #ggplot2::facet_wrap("DES_MPO_RES",scales = "free_y") +
+        ggplot2::theme(legend.position = c(.1, .85),
+                       legend.background = ggplot2::element_blank()) +
+        ggplot2::scale_color_manual(" ",
+                                    values = c("#D7DF71", "#0E5F76")) +
+        ggplot2::scale_fill_manual(" ",
+                                   values = c("#D7DF71", "#0E5F76")) +
+        ggplot2::scale_x_continuous(breaks = seq(from = 0,
+                                                 to = 52,
+                                                 by = 2)) +
+        ggplot2::ylab("Número de Casos") +
+        ggplot2::xlab("Semanas Epidemiológicas") +
+        ggplot2::theme(strip.text = ggplot2::element_text(size = 12),
+                       legend.background = ggplot2::element_blank(),
+                       legend.key = ggplot2::element_blank(),
+                       legend.box = ggplot2::element_blank())
+
+    #########
+
+
+    b <- densnv::read(path = path,
+                      vbd = "DEN",
+                      complete = TRUE) |>
+        dplyr::filter(ANO == year) |>
+        dplyr::filter(DES_MPO_REP %in% c(mpo)) |>
+        #dplyr::mutate(week = lubridate::epiweek(FEC_INI_SIGNOS_SINT)) |>
+        dplyr::group_by(DES_MPO_REP, SEM, ESTATUS_CASO) |>
+        dplyr::summarise(n = dplyr::n(),
+                         .groups = "drop") |>
+        tidyr::pivot_wider(id_cols = c(DES_MPO_REP, SEM),
+                           names_from = ESTATUS_CASO,
+                           values_fill = 0,
+                           values_from = n) |>
+        dplyr::rename(probable = `1`,
+                      confirmado =`2`,
+                      descartado = `3`) |>
+        dplyr::mutate(Porcentaje_Positividad = round((confirmado/(confirmado + descartado))*100,
+                                                     digits = 0),
+                      estimados = ((Porcentaje_Positividad/100) * probable) + confirmado) |>
+        dplyr::select(DES_MPO_REP, SEM,
+                      probable, confirmado,
+                      estimados) |>
+        tidyr::pivot_longer(cols = c(probable, confirmado,
+                                     estimados),
+                            names_to = "status",
+                            values_to = "n") |>
+        ggplot2::ggplot() +
+        ggplot2::geom_line(ggplot2::aes(x = SEM,
+                                        y = n,
+                                        col = status),
+                           linewidth = 2) +
+        ggplot2::geom_point(ggplot2::aes(x = SEM,
+                                         y = n,
+                                         fill = status),
+                            color = "white",
+                            shape = 21,
+                            size = 5) +
+        #ggplot2::facet_wrap("DES_MPO_RES",scales = "free_y") +
+        ggplot2::theme(legend.position = c(.1, .85),
+                       legend.background = ggplot2::element_blank()) +
+        ggplot2::scale_color_manual("",
+                                    values = c("#E01E5A", "#2EB67D", "#ECB32D")) +
+        ggplot2::scale_fill_manual("",
+                                   values = c("#E01E5A", "#2EB67D","#ECB32D")) +
+        ggplot2::scale_x_continuous(breaks = seq(from = 0,
+                                                 to = 52,
+                                                 by = 2)) +
+        ggplot2::theme(strip.text = ggplot2::element_text(size = 20),
+                       legend.background = ggplot2::element_blank(),
+                       legend.key = ggplot2::element_blank(),
+                       legend.box = ggplot2::element_blank()) +
+        ggplot2::scale_x_continuous(breaks = seq(from = 0,
+                                                 to = 52,
+                                                 by = 2)) +
+        ggplot2::ylab("Número de Casos") +
+        ggplot2::xlab("Semanas Epidemiológicas")
+
+
+
+
+    a / b
+
+}
